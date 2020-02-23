b1a768277b0ed9877e492dec29a2d1ab8a38d867
Tax CPAs <tatmoroz@msn.com>
2020-02-22 21:36:15 -0800

    Add .val() to js file to read inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-
+    var system = $("#system").val();
+    var end = $("input:radio[name=end]:checked").val();
+    var company = $("#company").val();
+    var criteria = $("#criteria").val();
+    var person = $("#person").val();
+    $('#result').show();
+    
+    if (system =="Desktop applications"){
+      
+      $('#ruby').show();
+   
+    }
     event.preventDefault();
   });
 });
