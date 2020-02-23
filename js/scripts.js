0d99f417294a89e40cab1d1d003faaf4f95f9d79
Tax CPAs <tatmoroz@msn.com>
2020-02-23 09:49:18 -0800

    Replace class names to make them more informative.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
-    var system = $("#system").val();
-    var end = $("input:radio[name=end]:checked").val();
-    var company = $("#company").val();
-    var criteria = $("#criteria").val();
-    var person = $("#person").val();
+    var system = $("#question-system").val();
+    var end = $("input:radio[name=question-end]:checked").val();
+    var company = $("#question-company").val();
+    var criteria = $("#question-criteria").val();
+    var person = $("#question-person").val();
     
-    $("#ruby").hide();
-    $("#jscript").hide();
-    $("#c").hide();
+    $("#language-ruby").hide();
+    $("#language-jscript").hide();
+    $("#language-c").hide();
 
     $('#result').show();
   
 
 
     if (system =="Desktop applications"&&criteria=="Language complexity"){
-      $('#ruby').show();
+      $('#language-ruby').show();
        } else if ((company=="Microsoft"||company=="Facebook")&&end=="front-end"){
-        $('#jscript').show();
+        $('#language-jscript').show();
        } else {
-        $('#c').show();
+        $('#language-c').show();
        }
     event.preventDefault();
     
